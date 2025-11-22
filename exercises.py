1cada2366e4f30c1188df271c42e7048dd604bdb
RawanAli <rawan.ali.fardan@email.com>
2025-11-22 12:17:22 +0300

    added function 5

--- exercises.py
+++ exercises.py
@@ -165,6 +165,51 @@ def weather_advice():
 # Call the function
 weather_advice()
 
+# Exercise 5: What's the Season?
+#
+# Write a Python function named `determine_season` that figures out the season based on the entered date.
+#
+# Requirements:
+# - The function should first prompt the user to enter the month (as three characters): "Enter the month of the year (Jan - Dec):"
+# - Then, the function should prompt the user to enter the day of the month: "Enter the day of the month:"
+# - Determine the current season based on the date:
+#      - Dec 21 - Mar 19: Winter
+#      - Mar 20 - Jun 20: Spring
+#      - Jun 21 - Sep 21: Summer
+#      - Sep 22 - Dec 20: Fall
+# - Print the season for the entered date in the format: "<Mmm> <dd> is in <season>."
+#
+# Hints:
+# - Use 'in' to check if a string is in a list or tuple.
+# - Adjust the season based on the day of the month when needed.
+# - Ensure to validate input formats and handle unexpected inputs gracefully.
+def determine_season():
+    month = input(" Enter the month's abbreviation ").capitalize()
+    day = int(input(" Enter day of month"))
+    valid_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    if month not in valid_months:
+        print("month not valid")
+        return
+    if day < 1 or day > 31:
+        print("day not valid")
+        return
+    # declar an empty variable
+    season = None
+    if (month == "Dec" and day >= 21) or month in ["Jan", "Feb"] or (month == "Mar" and day <= 19):
+        season = "winter"
+    elif (month == "Mar" and day >= 20) or month in ["Apr", "May"] or (month == "Jun" and day <= 10):
+        season = "spring"
+    elif (month == "Jun" and day >= 21) or month in ["Jul", "Aug"] or (month == "Sep" and day <= 21):
+        season = "Summer"
+    elif (month == "Sep" and day >= 22) or month in ["Oct", "Nov"] or (month == "Dec" and day <= 20):
+        season = "Fall"
+    if season:
+        print(f"{month} {day} is in {season}")
+    else:
+        print("unable to determine season")
+# Call the function
+determine_season()
+
 # Exercise 6: Number Guessing Game
 #
 # Write a Python function named `guess_number` that allows a user to guess a predetermined number within a range.
